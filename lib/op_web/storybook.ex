0f7932b70894021fe7cad75cf1192e439489c9dd
James Fiderlick <james@sixthedge.com>
2026-01-12 10:49:02 +0000

    chore: move storybook for Docker

--- lib/op_web/storybook.ex
+++ lib/op_web/storybook.ex
@@ -1,7 +1,7 @@
 defmodule OPWeb.Storybook do
   use PhoenixStorybook,
     otp_app: :op,
-    content_path: Path.expand("storybook", __DIR__),
+    content_path: Path.expand("../../storybook", __DIR__),
     # assets path are remote path, not local file-system paths
     css_path: "/assets/css/app.css",
     color_mode: true,
